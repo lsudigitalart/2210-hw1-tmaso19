4dc3ccfef93881ff4cfcfe9c0766a4aeba51b70e
“Ty <tmaso19@lsu.edu>
2024-08-28 15:08:17 -0500

    thing

---
+++ sketch.js
@@ -0,0 +1,36 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(196,250,255);
+  noStroke()
+  fill(250, 250, 250);
+  ellipse(220,height/2, 50, 50);
+  
+  fill(250, 250, 250);
+  ellipse(320,height/2, 50, 50);
+
+  fill(250, 250, 250);
+  ellipse(269,height/2.7, 50, 50);
+
+  fill(250, 250, 250);
+  ellipse(269,height/1.6, 50, 50);
+
+  fill(255, 255, 255);
+  ellipse(305,height/1.75, 50, 50);
+
+  fill(255, 255, 255);
+  ellipse(230,height/1.75, 50, 50);
+
+  fill(255, 255, 255);
+  ellipse(230,height/2.4, 50, 50);
+
+  fill(255, 255, 255);
+  ellipse(305,height/2.4, 50, 50);
+ 
+  fill(255, 255, 0);
+  noStroke();
+  ellipse(width/2,height/2, 60, 60);
+}
+
+function draw() {
+
+}
